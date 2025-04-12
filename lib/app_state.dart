ea29a14cba4b467580e33bdb7f79c98fe507edf0
LBidarra <up202307719@up.pt>
2025-04-12 11:44:29 +0100

    Added test to appstate

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -1,12 +1,13 @@
-import 'package:firebase_auth/firebase_auth.dart'
-    hide EmailAuthProvider;
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:learnvironment/firebase_options.dart';
 
 class ApplicationState extends ChangeNotifier {
-  ApplicationState() {
+  final FirebaseAuth _auth;
+
+  ApplicationState({FirebaseAuth? firebaseAuth}) : _auth = firebaseAuth ?? FirebaseAuth.instance {
     init();
   }
 
@@ -15,27 +16,21 @@ class ApplicationState extends ChangeNotifier {
 
   Future<void> init() async {
     try {
-      // Initialize Firebase
       await Firebase.initializeApp(
         options: DefaultFirebaseOptions.currentPlatform,
       );
+      FirebaseUIAuth.configureProviders([EmailAuthProvider()]);
     } catch (e) {
-      // Handle Firebase initialization error
       print("Error initializing Firebase: $e");
       return;
     }
 
-    // Set up Firebase UI providers
-    FirebaseUIAuth.configureProviders([EmailAuthProvider()]);
-
-    // Listen for authentication state changes
-    FirebaseAuth.instance.authStateChanges().listen((user) {
+    _auth.authStateChanges().listen((user) {
       _loggedIn = user != null;
-      notifyListeners(); // Notify listeners of authentication state change
+      notifyListeners();
     });
   }
 
-  // Method to manually update the logged-in status
   void updateLoggedInStatus(bool isLoggedIn) {
     _loggedIn = isLoggedIn;
     notifyListeners();
