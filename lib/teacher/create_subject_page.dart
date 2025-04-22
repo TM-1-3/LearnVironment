f16d2b4393ae816bd40048980d41f9699979b278
Maria Carneiro de Sousa <up202303834@up.pt>
2025-04-22 14:37:18 +0100

    subjects browser and creator
    # Conflicts:
    #	lib/main_pages/teacher_main_page.dart
    #	lib/teacher/widgets/subject_card.dart

---
+++ lib/teacher/create_subject_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CreateSubjectPage extends StatefulWidget {
+  const CreateSubjectPage({super.key});
+
+  @override
+  CreateSubjectPageState createState() => CreateSubjectPageState();
+}
+
+class CreateSubjectPageState extends State<CreateSubjectPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
