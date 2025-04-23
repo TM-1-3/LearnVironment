aa17f98293875f10c3156bbb0bb9ae96aba5dbc1
Henrique Vilarinho <henriquesantanavilarinho@gmail.com>
2025-04-23 11:30:24 +0100

    Subject data

--- lib/teacher/teacher_home.dart
+++ lib/teacher/teacher_home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:learnvironment/main_pages/games_page.dart';
-import 'package:learnvironment/main_pages/main_page.dart';
+import 'package:learnvironment/teacher/teacher_main_page.dart';
 import 'package:learnvironment/main_pages/profile_screen.dart';
 import 'package:learnvironment/main_pages/statistics_page.dart';
 
@@ -26,7 +26,7 @@ class _TeacherHomePage extends State<TeacherHomePage> {
 
     // Initialize the _pages map with the passed firestore and auth
     _pages[TabItem.statistics] = StatisticsPage();
-    _pages[TabItem.home] = MainPage();
+    _pages[TabItem.home] = TeacherMainPage();
     _pages[TabItem.games] = const GamesPage();
   }
 
