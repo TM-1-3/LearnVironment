e315b61df1a2bc17be633fc10a44438e36b245fd
TM-1-3 <tomasmorais2017@gmail.com>
2025-04-28 23:11:00 +0100

    Resolved merge mistake

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -306,24 +306,17 @@ class FirestoreService {
       if (turma== '') {
         throw Exception("No class selected");
       }
- messaging
       await _firestore.collection('events').add({
         'name': 'New Assignment!',
         'className': turma,
       });
       await _firestore.collection('assignment').add({
-
-      DocumentReference docRef = await _firestore.collection('assignment').add({
- main
         'title': title,
         'game_id': gameId,
         'class': turma,
         'dueDate': dueDate,
       });
-
-      String id = docRef.id;
       print("[FirestoreService] Created Assignment!");
-      return id;
     } catch (e) {
       print("[FirestoreService] Unable to create assignment!");
       throw Exception("Unable to create assignment!");
