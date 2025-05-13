50c7197258692f1ce2000c634bd216b7c0fc2207
Roque Crafter <up202304950@up.pt>
2025-05-13 12:21:37 +0100

    Fixed back buttons on Teacher Page

--- lib/student/student_main_page.dart
+++ lib/student/student_main_page.dart
@@ -80,6 +80,7 @@ class StudentMainPageState extends State<StudentMainPage> {
 
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: TextField(
           key: Key('search'),
           onChanged: (query) {
